3c213a429e531cb62707ef9caebc504c6bf5bcd9
kagrawal7 <kush.agr02@gmail.com>
2025-10-29 17:00:31 -0400

    added .gitignore and create team2 file with an idea for the workflow

---
+++ Team2/workflow_idea.py
@@ -0,0 +1,19 @@
+# https://www.geeksforgeeks.org/artificial-intelligence/alphago-algorithm-in-artificial-intelligence/
+
+# create agent class
+    # set up NN architecture, train and store weights
+        # Supervised learning policy network
+        # RL policy network
+        # value network
+    # implement MCTS
+
+# implement environment (doesnt have to be gymnasium; but it has to bridge Python chess and the agent)
+    # loop to for human to play the game against the agent
+    # methods to terminate the game
+    # print the current state
+    # print which pieces have been taken
+
+# (if we have time): A script that will initialize the agent and print the board at each turn
+
+# (maybe) some way of evaluating how good/bad our current solution is?
+# 
