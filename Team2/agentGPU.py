f06904d2be8f8894c852f936b1ea6267196f38c7
Masaru Morisako <morisako@dh2010pc07.utm.utoronto.ca>
2026-04-24 16:13:15 -0400

    temp save to pull

--- Team2/agentGPU.py
+++ Team2/agentGPU.py
@@ -10,32 +10,19 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model1 = SLPolicyValueNetwork().to(device)
-    # model1.load_state_dict(torch.load("checkpoint_stockfish_only.pth", map_location=torch.device("cuda"))["model"])
+    # model1.load_state_dict(torch.load("model_files/SL_stockfish_trained_old.pth", map_location=torch.device("cuda"))["model"])
+    model1.load_state_dict(torch.load("SL_trained_stockfish_trained.pth", map_location=torch.device("cuda"))["model"])
 
     # only train the first model
-    agent = Agent(policy_value_network=model1, c_puct=1.0, dirichlet_alpha=0.3, dirichlet_epsilon=0.25)
+    agent = Agent(policy_value_network=model1, c_puct=0.25, dirichlet_alpha=0.3, dirichlet_epsilon=0.25)
     # agent.training_self_play(num_training_iterations=1, num_games=4, train_to_test_ratio=0.8, num_simulations=10, resign_moves=20, resign_threshold=0.95, num_testing_games=20, improvement_threshold=30, temperature=0.1)
+
+    # example games
+    agent.agent_vs_stockfish(num_games=4, num_simulations=1000, path_to_output="pgn_files/examples.pgn", epoch=55)
 
     # progress too slow? not enough pitting games?
     # examples = agent.stockfish_self_play(2, 1.0)
     # print(examples[0])
 
     # train fresh network with stockfish
-    agent.stockfish_only_training(iterations=50, num_games=70, train_to_test_ratio=0.8, num_simulations=100, temperature=0.5, workers=14)
-
-    # # benchmark
-    # import time
-    # start = time.process_time()
-    # examples = []
-    # for _ in range(10):
-    #     examples = agent.stockfish_self_play(10)
-    # end = time.process_time()
-    # print(examples[0], end-start)
-
-
-    # torch.save(agent.policy_value_network.state_dict(), "lab_self_play.pth")
-    
-    
-    # test model performance
-    # pit_result = pit(model2, model1, num_games=10, num_simulations=1, c_puct=1.0, dirichlet_alpha=0.3, dirichlet_epsilon=0.25, temperature=0.1)
-    # print(f'{pit_result} games won out of {10}')
+    # agent.stockfish_only_training(iterations=200, num_games=200, train_to_test_ratio=0.8, num_simulations=100, temperature=0.5, workers=20)
