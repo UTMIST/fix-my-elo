981c7765b4b3721ee0c3a6107237c878492861f4
shrEkt101 <masarumorisako@gmail.com>
2026-07-06 13:47:05 -0400

    temp save

--- backend/api/agent_move/move.py
+++ backend/api/agent_move/move.py
@@ -1,29 +1,39 @@
 from fastapi import APIRouter, Response, HTTPException
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional, Annotated
+import os
 import chess
 
-router = APIRouter(
-    prefix="/agent_move"
-)
+router = APIRouter(prefix="/agent_move")
+
 
 class AgentMoveRequest(BaseModel):
     fen: Optional[str]
-    numSimulations: Optional[int]
-    temperature: Optional[int]
+    numSimulations: Optional[Annotated[int, Field(gt=2)]]
+    temperature: Optional[Annotated[float, Field(ge=0)]]
 
 
 @router.get("/")
 def test():
     return Response("routed to agent-move")
 
+
 @router.post("/")
 async def move_request(req: AgentMoveRequest):
-    
+
     try:
-        fen  = chess.Board(req.fen)
+        fen = chess.Board(req.fen)
     except:
         raise HTTPException(400, "invalid FEN")
     if not fen.is_valid():
         raise HTTPException(400, "invalid FEN")
-    
+
+    timeoutMs = os.environ.get("AGENT_MOVE_TIMEOUT_MS", default=1000)
+    engineUrl = os.envrion.get("TEAM2_ENGINE_URL", None)
+
+    if not engineUrl:
+        raise HTTPException(500, "Engine url not set")
+
+
+async def callEngineServer(req: AgentMoveRequest):
+    pass
