8f0a08884469e4a1436a2ddf43a386c065b48d09
shrEkt101 <masarumorisako@gmail.com>
2026-09-06 12:43:03 -0400

    CORS fix

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,7 +13,7 @@ load_dotenv(".env")
 app = FastAPI()
 
 app.include_router(agent_move_router)
-origins = [os.getenv("FRONTEND_URL"), "http://localhost:3000"]
+origins = [os.getenv("FRONTEND_URL"), "http://localhost:3000", "http://localhost:3100"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
